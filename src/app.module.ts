94f8ee62872f880500049ca2c8cb1f02009d09da
pelumini <pelumini@gmail.com>
2022-08-11 06:43:39 +0100

    added new modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,28 @@
 import { Module } from '@nestjs/common';
-import * as path from 'path';
 import { MongooseModule } from '@nestjs/mongoose';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { ConfigModule } from '@nestjs/config';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+// import { StripeModule } from './stripe/stripe.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
-    ConfigModule.forRoot(),
-    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
     MongooseModule.forRoot(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster0.mfnqe.azure.mongodb.net/foruchi?retryWrites=true&w=majority`,
     ),
     ProductModule,
+    UserModule,
+    AuthModule,
+    // StripeModule,
   ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
